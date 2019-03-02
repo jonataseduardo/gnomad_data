a3d26af7f58c1edc14bcf1ea44d308528f215bbf
jonataseduardo <jonataseduardo@gmail.com>
2019-03-01 23:04:21 -0300

    getting ancestral allele information from vep vcf

--- scripts/make_gnomad_freq_and_varlist.R
+++ scripts/make_gnomad_freq_and_varlist.R
@@ -5,11 +5,13 @@ if (length(args)==0) {
 } 
 
 input_vcf = args[1]
+vep_vcf = args[2]
 
 library(VariantAnnotation)
 library(data.table)
 
 #input_vcf <- "../gnomad_vcf/gnomad.exomes.r2.1.sites.chr22.head.vcf.bgz"
+#vep_vcf <- "/scratch/jonatas/vep_ancestral_alleles_vcf/homo_sapiens-chr22.vcf.gz"
 
 in_split <- unlist(strsplit(input_vcf, "/"))
 
@@ -17,6 +19,7 @@ file_name <- in_split[length(in_split)]
 prefix <- gsub("([^.+])\\.vcf\\.bgz$", "\\1", file_name)
 
 gad <- expand(readVcf(input_vcf), row.names = TRUE)
+vep <- expand(readVcf(vep_vcf), row.names = TRUE)
 
 info_gad <- info(gad)
 snps_gad <- rowRanges(gad)
@@ -62,6 +65,23 @@ setnames(pop_freqs,
         c("Chr", "Start", "End", "Ref", "Alt")
         )
 
+info_vep <- as.data.table(info(vep))
+snps_vep <- as.data.table(rowRanges(vep))
+
+info_vep[, idx := .I]
+snps_vep[, idx := .I]
+vep <- merge(snps_vep, info_vep[,.(AA, idx)], by = "idx")
+vep[, c('idx', 'width', 'strand', 
+        'paramRangeID', 'QUAL', 'FILTER') := NULL]
+
+setnames(vep, 
+        c("seqnames", "start", "end", "REF", "ALT"),
+        c("Chr", "Start", "End", "Ref", "Alt")
+        )
+
+setkeyv(vep, c("Chr", "Start", "End", "Ref", "Alt"))
+setkeyv(pop_freqs, c("Chr", "Start", "End", "Ref", "Alt"))
+
 system("mkdir -p ./gnomad_freqs")
 n_popinfo <- paste0("./gnomad_freqs/", prefix, ".freq.tsv")
-fwrite(pop_freqs, n_popinfo, sep = "\t", col.names = TRUE)
+fwrite(vep[pop_freqs], n_popinfo, sep = "\t", col.names = TRUE)
